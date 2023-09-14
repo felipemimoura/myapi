5f09a6ae93786fafa1941f0e92e9aa0a11ad36c4
Felipe Moura <felipemimoura@gmail.com>
2023-09-13 21:44:34 -0300

    add file to routes

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -2,17 +2,14 @@ import "dotenv/config";
 import express from "express";
 import "express-async-errors";
 import cors from "cors";
+import { routes } from "./routes";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Criando uma rota
-app.get("/", (request, response) => {
-  return response.json({
-    message: "Olá devs",
-  });
-});
+// Utlizando arquvio de rotas
+app.use(routes);
 
 app.listen(process.env.PORT, () => {
   console.log(`Server started on port ${process.env.PORT}!`);
