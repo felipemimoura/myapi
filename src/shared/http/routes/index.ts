5662154d18e873dc1b4b85f820cc1e16ff2ede59
Felipe Moura <felipemimoura@gmail.com>
2023-10-02 19:17:55 -0300

    feat: adicionado migration

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,45 +1,11 @@
 /*
  * TODO
-  - [x] Criar arquivo roles.routes.ts
-  - [x] Criar rota para criação de uma roles
-  - [x] Instalar UUID
-  - [x] Criar uma nova role
-  - [x] Retonar a rota criada
-  - [x] Criar arquivo roles dentro da pasta entities
-  - [x] Criar uma class Role
-  - [x] Atribuir o id através do construtor
-  - [x] utilizar  class Role para e juntar com as informações de nome e created_at
-  - [x] Criar RoleRepository dentro da pasta repository
-  - [x] Criar class RoleRepository
-  - [x] Criar método create na class RoleRepository
-  - [x] Criar DTO para método create
-  - [x] Refatorar rota create roposito
-  - [x] Criar método para buscar dados no banco de dados
-  - [x] Criar rota para buscar todos
-  - [x] Criar método findByName
-  - [x] Não permitir cadastrar um role com o mesmo nome
-  - [x] Criar Pastas useCases
-  - [x] Criar pasta creteRole para a criação de rotas
-  - [x] Criar arquivo Controller para criar a rota
-  - [x] Mover lógica para de criação do category
-  - [x] Utilizar classe de Error para quando a role já existir
-  - [x] Criar a useCase para CreateRole
-  - [x] Definir um DTO para a criação de uma role
-  - [x] Install swagger
-  - [x] Create file swagger.json on src
-  - [x] Import swagger-ui-express on server file
-  - [x] Import swagger file
-  - [x] Add middleware to use swagger
-  - [x]  Install typeOrm
-  - [x]  Install reflect-metadata
-  - [x]  Install sqlite3
-  - [x]  Import reflect-metadata in server.ts
-  - [x] Create file in shared/typeorm/index.ts
-  - [x] Initialize new DataSource file
-  - [x] add entities to dataSource
-  - [x] add migrations to dataSource
-  - [x] create a file app.ts and move all express configs
-
+  - [x] Criar script "typeorm" com as instruções "ts-node-dev -r tsconfig-paths/register ./node_modules/typeorm/cli.js"
+  - [x] Rodar a migration:create com o path "src/shared/typeorm/migration/CrateRolesTable"
+  - [x] Criar tabela com o nome: "Roles", columns: [{"id, uuid", "isPrimary: true"}, {"name", "string", "isUnique"}, {created_at, timestamp, default: CURRENT_TIMESTAMP}]
+  - [x] Drop table "Roles",
+  - [x] Adicionar classe a migration
+  - [] Rodar o script "npm run typeorm -- -d ./src/shared/typeorm/index.ts migration:run"
   */
 import { Router } from 'express'
 import { rolesRoutes } from '@roles/http/routes/roles.routes'
--- src/shared/typeorm/index.ts
+++ src/shared/typeorm/index.ts
@@ -1,8 +1,8 @@
 import { DataSource } from 'typeorm'
+import { CreateRolesTable1696193056260 } from './migration/1696193056260-CreateRolesTable'
 
 export const AppDataSource = new DataSource({
   type: 'sqlite',
   database: './db.sqlite',
-  entities: [],
-  migrations: [],
+  migrations: [CreateRolesTable1696193056260],
 })
